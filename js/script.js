065bff4a6b013f962a3c847e96927f54384dbe79
Erikson Oliveira <eriksonmagno6@hotmail.com>
2017-03-26 15:31:24 -0300

    Add menu for products

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+//products menu
+/*maskes sure that the first item is active on page load*/
+var classActive = 'active';
+
+$(".prod-menu-item").first().addClass(classActive);
+$(".prod-item").first().addClass(classActive);
+
+$(".prod-menu-item").click(function(e) {
+  e.preventDefault();
+  var itemId = $(this).attr("data-target");
+
+  /*remove class active from the previously
+  clicked item and adds to the current item*/
+  $(".prod-menu-item, .prod-item").removeClass(classActive);
+  $(this).addClass(classActive);
+  $(itemId).addClass(classActive);
+});
+
 //animate to the target section
 $("nav a").click(function(e) {
   e.preventDefault();
