b8db930f4f1732bf55b7f92d89efaae76275bbf1
Erikson Oliveira <eriksonmagno6@hotmail.com>
2017-03-26 10:46:35 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//animate to the target section
+$("nav a").click(function(e) {
+  e.preventDefault();
+
+  var id = $(this).attr("href"),
+      targetOffset = $(id).offset().top,
+      menuHeight = $("nav").innerHeight();
+
+  $("html, body").animate({
+    scrollTop: targetOffset - menuHeight
+  }, 500);
+});
+
+//animate sections on scroll
+(function() {
+  var $target = $('.wrapper'),
+      animationClass = 'anime-start',
+      offset = $(window).height() * 3/4;
+
+  function animeScroll() {
+    var documentTop = $(document).scrollTop();
+
+    $target.each(function() {
+      var itemTop = $(this).offset().top;
+      if (documentTop > itemTop - offset) {
+        $(this).addClass(animationClass);
+      }
+    })
+  }
+
+  animeScroll();
+
+  $(document).scroll(function() {
+    animeScroll();
+  })
+}());
